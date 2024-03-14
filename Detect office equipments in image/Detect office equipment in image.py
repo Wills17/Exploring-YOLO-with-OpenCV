6a46d67a17ef5eae0e083cc5ef9c6143e0c6c8a1
Williams Odunayo <157613997+Wills17@users.noreply.github.com>
2024-03-14 20:50:07 +0100

    Create Detect office equipment in image.py

---
+++ Detect office equipments in image/Detect office equipment in image.py
@@ -0,0 +1,44 @@
+from ultralytics import YOLO
+import cv2 as cv
+
+print("0\33c") #Clear screen
+
+# Define YOLO model
+model = YOLO("yolov8s-world.pt")
+
+# Read image
+image = cv.imread("Office equipment.jpg")
+image = cv.resize(image, (600,400))
+print("Image of office equipment has been produced")
+cv.imshow("Image showing equipments", image)
+
+# Store image in a variable - result
+result = model(image)
+print()
+
+# Collect and display results
+for i, r in enumerate(result):
+    detection = r.boxes.data.tolist()
+    names = r.names
+    classes = r.boxes.cls.tolist()
+    
+    # List to store detected people in images.
+    list = []
+    
+    for labels, detections in zip(classes, detection):
+            label = names[labels]
+            list.append(label)
+            
+            x,y,w,h,conf, _ = detections
+            # Round confidence value to 3 decimal places
+            conf = round(conf, 3)
+            
+            cv.putText(image, str(label), (int(x), int(y)), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 2, cv.LINE_AA)
+            cv.putText(image, str(conf), (int(x), int(h)), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1, cv.LINE_AA)
+            cv.rectangle(image, (int(x), int(y)), (int(w), int(h)), (0,100,0), 2)
+            
+        
+print("Total number of equipments detected in image:", len(list))
+cv.imshow("Image showing detected objects", image)
+cv.waitKey(0)
+cv.destroyAllWindows()
